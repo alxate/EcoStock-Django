82c6f215a6647562f6a37774f5e371c35e6f94b2
alxate <juan.jpav.79@gmail.com>
2025-04-09 15:21:42 -0500

    Inicializa proyecto EcoStock-Django: estructura básica, home view y admin configurado

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+
+def index(request):
+    contexto = {
+        'Titulo': 'Inicio',
+        'Mensaje': 'Controla tu inventario'
+    }
+    return render(request, 'main_app/index.html', contexto)
